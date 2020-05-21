36724ba02e4a090320b66ebdca878178f2403df9
José Ángel <josnih@gmail.com>
2020-05-21 19:05:20 +0100

    refactor

--- src/main/java/WordWrap.java
+++ src/main/java/WordWrap.java
@@ -1,25 +1,32 @@
 public class WordWrap {
     public static String wrapping(String text, int columnWidth) {
-        int newLineCount = 0;
-        String res = "";
-        if (text.length() < columnWidth) {
+
+        boolean shorterThanColumn = text.length() < columnWidth;
+        if (shorterThanColumn) {
             return text;
         }
 
-        if (text.length() > columnWidth) {
-            res += text.substring(0, columnWidth).concat("\n");
-            for (int i = 1; i <= text.length(); i++) {
-                if (i + columnWidth > text.length()) {
-                    res += text.substring(res.lastIndexOf("\n")-newLineCount);
-                    break;
-                } else if (i % columnWidth == 0) {
-                    res += text.substring(i, i + columnWidth).concat("\n");
-                    newLineCount++;
-                }
-            }
-            return res;
+        boolean biggerThanColumn = text.length() > columnWidth;
+        if (biggerThanColumn) {
+            return wrapString(text, columnWidth);
         }
         return "";
+    }
+
+    private static String wrapString(String text, int columnWidth) {
+        int newLineCount = 0;
+        String res = "";
+        res += text.substring(0, columnWidth).concat("\n");
+        for (int i = 1; i <= text.length(); i++) {
+            if (i + columnWidth > text.length()) {
+                res += text.substring(res.lastIndexOf("\n")-newLineCount);
+                break;
+            } else if (i % columnWidth == 0) {
+                res += text.substring(i, i + columnWidth).concat("\n");
+                newLineCount++;
+            }
+        }
+        return res;
     }
 
 }
