c46c168e943ea02ba6352bfe21adc7793539d34e
José Ángel <josnih@gmail.com>
2020-05-24 14:57:31 +0100

    restore project

---
+++ src/main/java/WordWrap.java
@@ -0,0 +1,26 @@
+public class WordWrap {
+    public static String wrapping(String text,int columnWidth){
+        String res = "";
+        if (text.length() < columnWidth){
+            return text;
+        }
+
+        if(text.length() > columnWidth){
+            res+= recursiveWrapping(text,columnWidth);
+            return res;
+            //return text.substring(0,columnWidth)+"\n"+text.substring(columnWidth,text.length());
+        }
+        return "";
+    }
+
+
+    public static String recursiveWrapping(String text,int columnWidth){
+        String finalString = text.substring(0,columnWidth)+"\n";
+        if(text.substring(columnWidth,text.length()).length()> columnWidth){
+          finalString += text.substring(finalString.length(),finalString.length()+columnWidth)+"\n";
+
+        }
+
+        return finalString;
+    }
+}
